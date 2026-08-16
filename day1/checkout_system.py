3a1db9b9abcb4207e484e73d372765ce927e6954
armad <8534425+armad@users.noreply.github.com>
2026-08-16 15:32:08 -0600

    Correct format aspects

--- day1/checkout_system.py
+++ day1/checkout_system.py
@@ -7,14 +7,15 @@ number_coffees = int(input("How many coffees did you buy? "))
 price = float(input("What's the price for each coffee? "))
 TAX_RATE = 0.12
 
-subtotal = number_coffees * price
+subtotal = round(number_coffees * price, 2)
 total = round(subtotal + (subtotal * TAX_RATE), 2)
 
 print("*" * 30)
 print("\tRECEIPT\t\n")
 print(f"Name: {name}\n")
 print("Breakdown\n")
-print(f"{number_coffees}x\t Coffee\t\t ${subtotal}\n")
+print("Qty\tItem\t\tCost")
+print(f"{number_coffees}\t Coffee\t\t ${subtotal}\n")
 print(f"Tax rate: {TAX_RATE * 100}%")
 print(f"TOTAL: ${total}")
 print("*" * 30)
